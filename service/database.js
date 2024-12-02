c3361d0dbde54bba84bf2ce79e1e185a26107cd1
Kayleigh Gustafson <92392059+kayleigh-gustafson@users.noreply.github.com>
2024-12-02 15:46:40 -0700

    Fixes for deploying login

--- service/database.js
+++ service/database.js
@@ -3,13 +3,12 @@ const bcrypt = require('bcrypt');
 const uuid = require('uuid');
 const config = require('./dbConfig.json');
 
-const url = `mongodb+srv://${config.userName}:${config.password}@${config.hostname}`;
+const url = `mongodb+srv://${config.userName}:${config.password}@${config.hostname}/?retryWrites=true&w=majority&appName=Cluster0`;
 const client = new MongoClient(url);
 const db = client.db('startup');
 const userCollection = db.collection('users');
 const dataCollection = db.collection('userdata');
 
-// This will asynchronously test the connection and exit the process if it fails
 (async function testConnection() {
   await client.connect();
   await db.command({ ping: 1 });
--- vite.config.js
+++ vite.config.js
@@ -6,4 +6,15 @@ export default defineConfig({
       '/api': 'http://localhost:3000',
     },
   },
+  build: {
+    chunkSizeWarningLimit: 100,
+    rollupOptions: {
+      onwarn(warning, warn) {
+        if (warning.code === "MODULE_LEVEL_DIRECTIVE") {
+          return;
+        }
+        warn(warning);
+      },
+    },
+  },
 });
